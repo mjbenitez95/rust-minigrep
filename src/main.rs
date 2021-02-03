7f310898243f67499b2000909984d2e706279c72
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-02-03 09:48:52 -0800

    further improve error handling using unwrap_or_else()

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!(
         "This program will search for \"{}\" in file {}",
@@ -13,7 +17,10 @@ fn main() {
     let contents =
         fs::read_to_string(&config.filename).expect("Something went wrong with reading the file");
 
-    println!("The file {} has contents: \n\n{}", &config.filename, contents);
+    println!(
+        "The file {} has contents: \n\n{}",
+        &config.filename, contents
+    );
 }
 
 struct Config {
@@ -22,13 +29,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("Not enough arguments passed into the program!");
+            return Err("Not enough arguments.");
         }
+
         let query = args[1].clone();
         let filename = args[2].clone();
-    
-        Config { query, filename }
+
+        Ok(Config { query, filename })
     }
 }
