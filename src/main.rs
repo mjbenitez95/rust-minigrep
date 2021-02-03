6e4b60596cb6b008781553f1983bef9bd3bc4533
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-02-03 09:27:52 -0800

    save argument values into variables

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,12 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("Arguments are: {:?}!", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!(
+        "This program will search for \"{}\" in file {}",
+        query, filename
+    );
 }
