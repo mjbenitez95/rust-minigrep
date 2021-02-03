5aa669e9e50f095019db99c577d7e77e91b65a03
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-02-03 09:56:19 -0800

    move program logic into lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use std::error::Error;
-use std::{env, fs, process};
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
@@ -13,37 +12,8 @@ fn main() {
         config.query, config.filename
     );
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}.", e);
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(&config.filename)?;
-
-    println!(
-        "The file {} has contents: \n\n{}",
-        &config.filename, contents
-    );
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments.");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
     }
 }
