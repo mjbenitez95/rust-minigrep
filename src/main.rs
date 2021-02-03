b6b2d9901aaf15e5da185d7002c511a91a58d779
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-02-03 09:39:46 -0800

    refactor to group arguments into Config struct

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,27 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    let query = &args[1];
-    let filename = &args[2];
+    let config = parse_config(&args);
 
     println!(
         "This program will search for \"{}\" in file {}",
-        query, filename
+        config.query, config.filename
     );
 
     let contents =
-        fs::read_to_string(filename).expect("Something went wrong with reading the file");
+        fs::read_to_string(&config.filename).expect("Something went wrong with reading the file");
 
-    println!("The file {} has contents: \n\n{}", filename, contents);
+    println!("The file {} has contents: \n\n{}", &config.filename, contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
