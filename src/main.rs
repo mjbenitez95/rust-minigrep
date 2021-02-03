96047ef10fbd2ad654941983c3f433bbeb4b0eb5
Matthew Benitez <matthewjbenitez95@gmail.com>
2021-02-03 09:42:29 -0800

    fold parse_configs method into Config struct as implementation of constructor

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!(
         "This program will search for \"{}\" in file {}",
@@ -21,9 +21,11 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Config { query, filename }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+    
+        Config { query, filename }
+    }
 }
